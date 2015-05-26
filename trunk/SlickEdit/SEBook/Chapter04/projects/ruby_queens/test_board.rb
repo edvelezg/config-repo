0ce832d4c5af7bfdd660f9754d0369129d234660
EdVelez.G@gmail.com <EdVelez.G@gmail.com@238975a8-dd92-959c-bb25-6076613380b5>
2015-05-26 04:39:49 +0000

    chgd requires

--- trunk/SlickEdit/SEBook/Chapter04/projects/ruby_queens/test_board.rb
+++ trunk/SlickEdit/SEBook/Chapter04/projects/ruby_queens/test_board.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'test/unit'
-require 'board'
+require_relative 'test/unit'
+require_relative 'board'
 
 class TestBoard < Test::Unit::TestCase
   def test_create
