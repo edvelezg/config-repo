2080276d63be6917b87e71a7a8f30c052f35e808
EdVelez.G@gmail.com <EdVelez.G@gmail.com@238975a8-dd92-959c-bb25-6076613380b5>
2015-05-31 00:52:16 +0000

    WIP More failed attempts

--- trunk/SlickEdit/SEBook/Chapter04/projects/gnu_queens/queens.cpp
+++ trunk/SlickEdit/SEBook/Chapter04/projects/gnu_queens/queens.cpp
@@ -20,22 +20,23 @@
 #include <iostream>
 #include <string>
 #include "board.h"
+#include <cstdlib>
 
-int main (int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cerr << "I need a number." << std::endl;
-    exit(-1);
-  }
-  int size = atoi(argv[1]);
-  Board b(size);
-  time_t start = time(0);
-  if (b.solve()) {
-    time_t elapsed = time(0) - start;
-    for (int i = 0; i < size; i++) {
-      std::cout << std::string(b.col(i), ' ') << "*" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "I need a number." << std::endl;
+        exit(-1);
     }
-    std::cout << elapsed << " seconds." << std::endl;
-  }
-  return(0);
+    int size = atoi(argv[1]);
+    Board b(size);
+    time_t start = time(0);
+    if (b.solve()) {
+        time_t elapsed = time(0) - start;
+        for (int i = 0; i < size; i++) {
+            std::cout << std::string(b.col(i), ' ') << "*" << std::endl;
+        }
+        std::cout << elapsed << " seconds." << std::endl;
+    }
+    return (0);
 }
 
