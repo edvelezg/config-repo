4b2a4975ca4f0ce733c1f13f8d62f1f22191a439
Ed <edvelez.g@gmail.com>
2018-02-17 09:56:37 -0600

    one place for .aliases and .functions

---
+++ trunk/dev_scripts/ruby/BuildMirrorProject/download.bat
@@ -0,0 +1,10 @@
+rem git show HEAD:trunk/MyBashProfile/ROFA/.aliases > .aliases
+git show HEAD:trunk/MyBashProfile/ROFA/.aliases        >   .aliases      
+git show HEAD:trunk/MyBashProfile/ROFA/.bash_profile   >   .bash_profile 
+git show HEAD:trunk/MyBashProfile/ROFA/.functions      >   .functions    
+
+
+
+
+
+
