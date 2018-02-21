a60f1bf428770d9b732ce82f45baf79b2290bad3
EdGV <egutarra@tibco.com>
2018-02-21 12:32:25 -0600

    renamed to rsync.ini

--- trunk/dev_scripts/ruby/BuildMirrorProject/download.bat
+++ trunk/dev_scripts/ruby/BuildMirrorProject/download.bat
@@ -1,14 +1,6 @@
-rem git show HEAD:trunk/MyBashProfile/ROFA/.aliases > .aliases
-
-echo adding git to the path
-SET PATH="%PATH%;C:\Program Files\Git\bin\"
+cd C:\Users\egutarra\config-repo2\trunk\dev_scripts\ruby\BuildMirrorProject
 
 git show HEAD:trunk/MyBashProfile/ROFA/.aliases        >   .aliases      
 git show HEAD:trunk/MyBashProfile/ROFA/.bash_profile   >   .bash_profile 
 git show HEAD:trunk/MyBashProfile/ROFA/.functions      >   .functions    
-
-
-
-
-
 
